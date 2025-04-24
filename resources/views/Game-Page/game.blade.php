e139a14e9723f4c11fccf107e4f4f09d52a34a53
Murilo Luis Iossi Lopes <murilo.lopes32@etec.sp.gov.br>
2025-04-24 08:58:19 -0300

    adicionado o layout.blade.php nas views e diretivas feitas nas views

--- resources/views/Game-Page/game.blade.php
+++ resources/views/Game-Page/game.blade.php
@@ -1,13 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-</head>
-<body>
-    <div class="bg-secondary" style="width: 800px; height: 600px;"></div>
-</body>
-</html>
+@extends('layout')
+
+@section('content')
+<div class="bg-secondary" style="width: 800px; height: 600px;"></div>
+@endsection
 <!--este código é um exemplo, futuramente o jogo virá aqui.-->
