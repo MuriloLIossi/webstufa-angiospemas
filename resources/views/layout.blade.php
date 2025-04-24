c175ff1dfb23894464c96624af843315fe358051
Murilo Luis Iossi Lopes <murilo.lopes32@etec.sp.gov.br>
2025-04-24 09:15:22 -0300

    adicionada nav bar no layout

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -7,6 +7,24 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   </head>
   <body class="container mt-4 bg-dark text-light p-4 rounded">
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark  ">
+  <div class="collapse navbar-collapse  justify-content-center" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Features</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Pricing</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+      </li>
+    </ul>
+  </div>
+</nav>
     <main class="container">
       @yield('content')
     </main>
