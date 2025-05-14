711a8b580d565878ebf36987694cc774e28b8e0a
Murilo Luis Iossi Lopes <murilo.lopes32@etec.sp.gov.br>
2025-05-14 10:25:29 -0300

    seeder agr funciona direito

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+
+        $this->call([
+        GallerySeeder::class,
+        ]);
         // // User::factory(10)->create();
 
         // User::factory()->create([
